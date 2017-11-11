0e3389c1cc086e23d3472e7a881c85274a471fbc
Vicente Rotman <vrotmanh@gmail.com>
2017-11-11 17:55:53 -0500

    Add title, context texts and footer

--- App.js
+++ App.js
@@ -1,23 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default class App extends React.Component {
+
+class LoginInput extends Component {
+  render(){
+    return (
+      <Text>Enter your {this.props.label}!</Text>
+    );
+  }
+
+}
+
+export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <View>
+          <Text style={styles.headerText}>Login MOVES</Text>
+        </View>
+        <View style={styles.content}>
+          <LoginInput label='email' />
+          <LoginInput label='password' />
+        </View>
+        <View>
+          <Text style={styles.footerText}>Register</Text>
+        </View>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
+  headerText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#205166'
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
   },
+  footerText: {
+    fontSize: 15,
+    fontWeight: 'bold',
+  }
 });
