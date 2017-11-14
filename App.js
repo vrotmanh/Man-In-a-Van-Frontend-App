8e091ed4c0a52e3ad054e538bc32af8d19c7ca33
Vicente Rotman <vrotmanh@gmail.com>
2017-11-13 20:31:11 -0500

    Login view

--- App.js
+++ App.js
@@ -1,28 +1,43 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, Alert } from 'react-native';
 
 
 class LoginInput extends Component {
   render(){
     return (
-      <Text>Enter your {this.props.label}!</Text>
+      <TextInput
+        style={styles.loginInput}
+        placeholder={this.props.label}
+      />
     );
   }
 
 }
 
 export default class App extends Component {
+
+  _login(event){
+    console.log(event)
+    Alert.alert('on Press!');
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <View>
+        <View style={styles.header}>
           <Text style={styles.headerText}>Login MOVES</Text>
         </View>
         <View style={styles.content}>
-          <LoginInput label='email' />
-          <LoginInput label='password' />
+          <LoginInput label='Enter your email' />
+          <LoginInput label='Enter your password' />
+          <Button
+            onPress={this._login}
+            title="Login"
+            color="#841584"
+            accessibilityLabel="Login"
+          />
         </View>
-        <View>
+        <View style={styles.footer}>
           <Text style={styles.footerText}>Register</Text>
         </View>
       </View>
@@ -31,20 +46,39 @@ export default class App extends Component {
 }
 
 const styles = StyleSheet.create({
-  headerText: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    color: '#205166'
-  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
+    alignSelf: "center"
+  },
+  header : {
+    flex: 1,
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
+  headerText: {
+    fontSize: 26,
+    alignItems: 'center',
+    fontWeight: 'bold',
+    color: '#205166'
+  },
+  content: {
+    flex: 2,
+    marginTop: 50,
+    alignItems: 'center',
     flexDirection: 'column',
-    justifyContent: 'space-between',
+  },
+  loginInput: {
+    fontSize: 18,
+  },
+  footer: {
+    flex: 1,
+    alignItems: 'center',
+    flexDirection: 'row',
   },
   footerText: {
-    fontSize: 15,
+    fontSize: 20,
     fontWeight: 'bold',
   }
 });
